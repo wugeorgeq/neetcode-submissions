91ba8ed05592a62df2b97f168bcb661192c01e56
George Wu <3665262+wugeorgeq@users.noreply.github.com>
2026-05-17 10:01:00 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.kt
@@ -0,0 +1,31 @@
+class Solution {
+
+    fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+        var prev1 = nums[0]
+        var prev2 = maxOf(nums[0], nums[1])
+        for (i in 2 until nums.size) {
+            val current = maxOf(prev1 + nums[i], prev2)
+            prev1 = prev2
+            prev2 = current
+        }
+        return maxOf(prev1, prev2)
+    }
+    /**
+    probably something like dp[n] is max amount at index n
+    at each index n, dp[n] = max(dp[n-1], nums[n] + dp[n-2])
+
+    oh wait let's do the non array version too
+    fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+        val dp = IntArray(nums.size)
+        dp[0] = nums[0]
+        dp[1] = maxOf(nums[0], nums[1])
+        for (i in 2 until nums.size) {
+            dp[i] = maxOf(dp[i-1], nums[i] + dp[i-2])
+        }
+        return dp[nums.size - 1]
+    }
+
+    */
+}
