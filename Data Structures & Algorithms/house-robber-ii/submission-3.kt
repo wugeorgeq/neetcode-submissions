84ffbda80d878767519f2417c106bc7074d5ac8e
George Wu <3665262+wugeorgeq@users.noreply.github.com>
2026-05-17 10:15:22 -0700

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.kt
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+    do i just run house robbers algo on the array except the last index
+    and except the first index?
+    */
+    fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+        return maxOf(rob(nums, 1, nums.size), rob(nums, 0, nums.size - 1))
+    }
+
+    // 1, 3
+    fun rob(nums: IntArray, startIndex: Int, endIndex: Int): Int {
+        var prev2 = 0
+        var prev1 = 0
+        for (i in startIndex until endIndex) {
+            val current = maxOf(prev2 + nums[i], prev1)
+            prev2 = prev1
+            prev1 = current
+        }
+        return maxOf(prev2, prev1)
+    }
+}
